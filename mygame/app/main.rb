70a4ee5d71ffb57f0958d384a19b9c9851974acf
Ryan V <47340962+denvermullets@users.noreply.github.com>
2023-01-23 08:06:30 -0500

    chore(main): clean up comments (#3)

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,35 +1,7 @@
 require 'app/my_game.rb'
 
 def tick(args)
-  # args.state.player ||= {
-  #   x: args.grid.w / 2,
-  #   y: args.grid.h / 2,
-  #   w: 20,
-  #   h: 20
-  # }
-
-  # speed = 12
-
-  # if args.inputs.left
-  #   args.state.player.x -= speed
-  # elsif args.inputs.right
-  #   args.state.player.x += speed
-  # elsif args.inputs.down
-  #   args.state.player.y -= speed
-  # elsif args.inputs.up
-  #   args.state.player.y += speed
-  # end
-
-  # args.outputs.sprites << [
-  #   args.state.player.x,
-  #   args.state.player.y,
-  #   100,
-  #   80,
-  #   'sprites/misc/dragon-0.png'
-  # ]
   $my_game ||= MyGame.new(args)
   $my_game.args = args
   $my_game.tick
-
-
 end
