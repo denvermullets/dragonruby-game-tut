5367193be29d48b9e39cf74cbfd658b37b48cc5b
Ryan V <47340962+denvermullets@users.noreply.github.com>
2023-01-23 08:04:33 -0500

    chore(player): refactor to use classes (#2)

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,6 +1,6 @@
+require 'app/my_game.rb'
+
 def tick(args)
-  args.state.player_x ||= 120
-  args.state.player_y ||= 280
   # args.state.player ||= {
   #   x: args.grid.w / 2,
   #   y: args.grid.h / 2,
@@ -8,31 +8,8 @@ def tick(args)
   #   h: 20
   # }
 
-  speed = 12
+  # speed = 12
 
-  player_w = 100
-  player_h = 80
-
-  if args.inputs.left
-    args.state.player_x -= speed
-  elsif args.inputs.right
-    args.state.player_x += speed
-  end
-
-  if args.inputs.up
-    args.state.player_y += speed
-  elsif args.inputs.down
-    args.state.player_y -= speed
-  end
-
-  # part of keeping the sprite within boundaries of the window
-  args.state.player_x + player_w > args.grid.w && args.state.player_x = args.grid.w - player_w
-  args.state.player_x.negative? && args.state.player_x = 0
-
-  args.state.player_y + player_h > args.grid.h && args.state.player_y = args.grid.h - player_h
-  args.state.player_y.negative? && args.state.player_y = 0
-
-  # Move player based on keyboard input
   # if args.inputs.left
   #   args.state.player.x -= speed
   # elsif args.inputs.right
@@ -43,11 +20,16 @@ def tick(args)
   #   args.state.player.y += speed
   # end
 
-  args.outputs.sprites << [
-    args.state.player_x,
-    args.state.player_y,
-    100,
-    80,
-    'sprites/misc/dragon-0.png'
-  ]
+  # args.outputs.sprites << [
+  #   args.state.player.x,
+  #   args.state.player.y,
+  #   100,
+  #   80,
+  #   'sprites/misc/dragon-0.png'
+  # ]
+  $my_game ||= MyGame.new(args)
+  $my_game.args = args
+  $my_game.tick
+
+
 end
---
+++ mygame/app/player.rb
@@ -0,0 +1,10 @@
+class Player
+  attr_sprite
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @w = 100
+    @h = 80
+  end
+end
