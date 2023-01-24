a5184cafdba3b1e93605656110836a93b666ec5d
denvermullets <devmullets@gmail.com>
2023-01-24 07:27:21 -0500

    feat(player): adds ability to shoot fireballs

--- mygame/app/my_game.rb
+++ mygame/app/my_game.rb
@@ -1,7 +1,7 @@
 require 'app/player.rb'
 
 class MyGame
-  # game specific attr methods
+  # deconstructs game specific attr methods
   attr_gtk
   attr_reader :player
 
@@ -12,11 +12,19 @@ class MyGame
   end
 
   def tick
+    args.state.fireballs ||= []
+
     handle_input
     render
   end
 
   def handle_input
+    player_movement
+    shoot_fireball
+  end
+
+  def player_movement
+    # player_movement and boundary checks could be moved to module/service object
     if keyboard.left
       player.x -= 10
     elsif keyboard.right
@@ -26,16 +34,46 @@ class MyGame
     elsif keyboard.up
       player.y += 10
     end
+
+    check_boundaries
+  end
+
+  def shoot_fireball
+    # unsure if ultimately this _should_ be inside Player, but leaving it outside for now
+    return unless keyboard.key_down.z || keyboard.key_down.j || inputs.controller_one.key_down.a
+
+    state.fireballs << {
+      w: 32,
+      h: 32,
+      x: player.x,
+      y: player.y,
+      path: 'sprites/fireball.png'
+    }
+  end
+
+  def check_boundaries
+    check_x_border
+    check_y_border
+  end
+
+  def check_x_border
+    player.x + player.w > grid.w && player.x = grid.w - player.w
+    player.x.negative? && player.x = 0
+  end
+
+  def check_y_border
+    player.y + player.h > grid.h && player.y = grid.h - player.h
+    player.y.negative? && player.y = 0
+  end
+
+  def move_fireball
+    state.fireballs.each do |fireball|
+      fireball.x += player.speed
+    end
   end
 
   def render
-    # x, y, w, h, source
-    outputs.sprites << {
-      x: player.x,
-      y: player.y,
-      w: player.w,
-      h: player.h,
-      path: 'sprites/misc/dragon-0.png'
-    }
+    move_fireball
+    outputs.sprites << [player, state.fireballs]
   end
 end
--- mygame/app/player.rb
+++ mygame/app/player.rb
@@ -1,10 +1,13 @@
 class Player
   attr_sprite
+  attr_accessor :speed
 
   def initialize(x, y)
     @x = x
     @y = y
     @w = 100
     @h = 80
+    @path = 'sprites/misc/dragon-0.png'
+    @speed = 2
   end
 end
